455f19946a627d9954f332d35c20d9747308c8e1
Muhammad Hassan Arshad <muhammadhassanarshad@Muhammad-Hassan-Arshad.local>
2019-11-07 03:18:11 +0500

    Fixed Camera

--- components/Camera/Camera.js
+++ components/Camera/Camera.js
@@ -1,10 +1,11 @@
 import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
 import React, { Component } from 'react';
-import { ActivityIndicator, Icon, Item, Dimensions, Platform, View, TextInput, TouchableOpacity, TouchableHighlight, Text, KeyboardAvoidingView, Image } from 'react-native';
+import { Button, ActivityIndicator, Icon, Item, Dimensions, Platform, View, TextInput, TouchableOpacity, TouchableHighlight, Text, KeyboardAvoidingView, Image } from 'react-native';
 import { Ionicons, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
 import { ImageManipulator } from 'expo';
 const axios = require('axios')
+import * as ImagePicker from 'expo-image-picker';
 
 const imageWidth = Dimensions.get('window').width;
 const imageHeight = Dimensions.get('window').height;
@@ -35,109 +36,78 @@ class CameraComponent extends Component {
         }
     }
 
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' })
+  async componentWilllMount() {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
   }
 
-  async snapPhoto() {     
-    console.log('Button Pressed');
-    if (this.camera) {
-      this.camera.stopRecording()
-      this.setState({ loading: true })
-      console.log('Taking photo');
-      const data = await this.camera.takePictureAsync()
-      console.log(data.uri)
-      this.setState({ image: data.uri})
-      img_type = ((data.uri).split(".").pop())
-      img_type = "jpg"
-      const type_ = "image/" + img_type;
-      const name_ = "photo." + img_type;
+    _pickImage = async () => {
+      console.log("HERE")
+      let result = await ImagePicker.launchCameraAsync({})
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+        console.log(this.state.image)
 
-      console.log(name_)
-      console.log(type_)
-      console.log("-------------")
+        img_type = ((this.state.image).split(".").pop())
+        img_type = "jpg"
+        const type_ = "image/" + img_type;
+        const name_ = "photo." + img_type;
 
-      const formData = new FormData();
-      const photo = {
-        uri: data.uri,
-        type: type_,
-        name: name_
+        console.log(name_)
+        console.log(type_)
+        console.log("-------------")
+
+        const formData = new FormData();
+        const photo = {
+          uri: this.state.image,
+          type: type_,
+          name: name_
+        }
+
+        formData.append('image', photo)
+      
+        console.log(formData)
+
+        const res = await axios.post('http://10.130.96.240:5000/', formData, {
+            headers: {
+              'content-type': `multipart/form-data`,
+            }
+        }).then(function (response) {
+          console.log(response.data['score']);
+        }).catch(function (err) {
+          console.log(err);
+        });
+
+        this.setState({ loading: false })
+        this.setState({ image: null })
+
+        this._pickImage()
+        
       }
-
-      formData.append('image', photo)
-    
-      console.log(formData)
-
-      const res = await axios.post('http://10.130.96.240:5000/', formData, {
-          headers: {
-            'content-type': `multipart/form-data`,
-          }
-      }).then(function (response) {
-        console.log(response.data['score']);
-      }).catch(function (err) {
-        console.log(err);
-      });
-
-      this.setState({ loading: false })
-      this.setState({ image: null})
-
-     }
     }
 
   render() {
 
     const { hasCameraPermission } = this.state
 
-    if (hasCameraPermission === null) {
+    if (0) {
       return <View />
     }
-    else if (hasCameraPermission === false) {
+    else if (0) {
       return <Text>No access to camera</Text>
     }
     else {
       return (
-        <View style={{ 
-        	flex: 1,
-          height: imageHeight,
-          width: imageWidth,
-		    flexDirection: 'column',
-         }}>
-          <Camera ref={ref => {
-              this.camera = ref;
-            }} 
-            type={this.state.type}
-            autoFocus={this.state.focus}
-            onCameraReady={this.prepareRatio}
-            ratio= {this.state.r}
-            style = {{
-	          	flex: 1,
-              justifyContent: 'flex-end',
-              alignItems: 'center'
-          }}>
-            <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}} >
-              <TouchableOpacity
-                style={{
-                  	flex: 0,
-                    padding: 15,
-                    paddingHorizontal: 20,
-                    alignSelf: 'center',
-                    margin: 20,
-                    borderWidth:8,
-                    borderColor:'rgba(0,0,0,0.5)',
-                    alignItems:'center',
-                    justifyContent:'center',
-                    width:75,
-                    height:75,
-                    backgroundColor:'#fff',
-                    borderRadius:75,
-                }}
-                onPress={this.snapPhoto.bind(this)}
-                >
-              </TouchableOpacity>
-            </View>
-          </Camera>
-          {this.state.loading && this.state.image &&
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {!this.state.image && <Button
+            title="Tap to Launch Camera"
+            onPress={this._pickImage}
+          />}
+          {this.state.image &&
                               <View>
                                 <Image
                                   style={{width: imageWidth, height: imageHeight}}
@@ -150,10 +120,10 @@ class CameraComponent extends Component {
                                   paddingLeft: 0,
                                   alignItems: 'center',
                                   justifyContent: 'center',
-                                  opacity: 0.8,
+                                  opacity: 0.7,
                                   backgroundColor: '#808080',
                                 }}>
-            <ActivityIndicator style= {{paddingBottom: imageHeight/5}} color='#316538' size='large'/>
+            <ActivityIndicator style= {{alignSelf: 'center'}} color='#FFFFFF' size='large'/>
             </View>
           </View>}
         </View>
