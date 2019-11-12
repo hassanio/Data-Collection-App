b11d52a5646311539678d97aa7fd71308d4bd4d5
Raza Khawaja <razakhawaja@Raza-Khawaja.local>
2019-11-13 03:12:36 +0500

    Fixed Flask Error

---
+++ components/Camera/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+	FirebaseConfig: {
+		apiKey: "",
+	    authDomain: "",
+	    databaseURL: "",
+	    projectId: "",
+	    storageBucket: "gs://sproj-soil-d1d39.appspot.com",
+	    messagingSenderId: ""
+	}
+}
